a8fdcdc6196ed5a1ccff5a5dc50f467f99dfe90e
Lydiksen <rasmus.lydiksen@gmail.com>
2014-10-13 18:09:10 +0200

    Interface forbindelser + interfaces

--- src/com/example/tictactoe/SinglePlayerGameActivity.java
+++ src/com/example/tictactoe/SinglePlayerGameActivity.java
@@ -1,7 +1,9 @@
 package com.example.tictactoe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SinglePlayerGameActivity extends Activity 
 {
@@ -10,7 +12,28 @@ public class SinglePlayerGameActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) 
 	{
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_single_player_game);
+        
+        Intent gameIntent = getIntent();
+        
+        String msg = gameIntent.getStringExtra(SinglePlayerOpponentActivity.GameType_Message);
+        int gameTypeNumber = gameIntent.getIntExtra(SinglePlayerOpponentActivity.GameType_Message,1);
+        TextView opponentText = (TextView) findViewById(R.id.gameType2);
+        
+        if(gameTypeNumber == 1)
+        {
+        	
+        	opponentText.setText("Random opponent");
+        }
+        else
+        {
+        	opponentText.setText("Terminator Opponent");
+        }
+        
+        
+        
+        
+        //setContentView(txtView);        
         
     }
 }
