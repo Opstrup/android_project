83f2c025dc05f81bea32e8cb311d843441005883
Lydiksen <rasmus.lydiksen@gmail.com>
2014-10-14 15:38:05 +0200

    singleplayer opdateret

--- src/com/example/tictactoe/SinglePlayerGameActivity.java
+++ src/com/example/tictactoe/SinglePlayerGameActivity.java
@@ -7,18 +7,24 @@ import com.example.tictactoe.GameLogic.RandomBot;
 import com.example.tictactoe.GameLogic.TicTacToeGame;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SinglePlayerGameActivity extends Activity 
 {
     ITicTacToeGame theGame;
     IBot theBot;
-    int Tic_or_Toe;
+    int Tic_or_Toe, move;
+    boolean PlayerMove = true;
+    ImageButton image;
+    int imagearray[] = {R.id.ImageButton00,R.id.ImageButton01,R.id.ImageButton02,R.id.ImageButton03,R.id.ImageButton04,R.id.ImageButton05,R.id.ImageButton06,R.id.ImageButton07,R.id.ImageButton08};
     
 	@Override
     protected void onCreate(Bundle savedInstanceState) 
@@ -40,55 +46,147 @@ public class SinglePlayerGameActivity extends Activity
         }
         else
         {
-        	
         	opponentText.setText("Terminator Opponent");
-        }
+        } 
     }
 	
-	public void GameLoop()
-	{
-//		while (theGame.HasWinner() == false && theGame.BoardIsFull == false) 
-//		{
-//			while(PlayerMove == false);
-//			
-//			
-//		}
-	}
+	
 	
 	public void SetButton(View view) 
 	{
 		
-		if (theGame.whosTurn() == BoardFieldEnum.PLAYER_O)
-		{
-			Tic_or_Toe = R.drawable.bolle;
-		}
-		else
-		{
-			Tic_or_Toe = R.drawable.kryds;
-		}
+//		if (theGame.whosTurn() == theGame.getPlayerType())
+//		{
+//			Tic_or_Toe = R.drawable.bolle;
+//		}
+//		else
+//		{
+//			Tic_or_Toe = R.drawable.kryds;
+//		}
 				
 		switch (view.getId()) {
 		case R.id.ImageButton00:
-			ImageButton image00 = (ImageButton) findViewById(R.id.ImageButton00);
-//			if( theGame.updateBoard(0) == True) 
-//			{ 
-//				image00.setImageResource(Tic_or_Toe);
-//				
-//			}
-		
-
+			image = (ImageButton) findViewById(R.id.ImageButton00);
+			if( theGame.UpdateBoard(0) == true) 
+			{ 
+				image.setImageResource(Tic_or_Toe);
+								
+			}
 			break;
 			
 		case R.id.ImageButton01:
-			ImageButton image01 = (ImageButton) findViewById(R.id.ImageButton01);
-			image01.setImageResource(R.drawable.kryds);
+			image = (ImageButton) findViewById(R.id.ImageButton01);
+			if( theGame.UpdateBoard(1) == true) 
+			{ 
+				image.setImageResource(Tic_or_Toe);
+				
+			}
 			break;
 		
+		case R.id.ImageButton02:
+			image = (ImageButton) findViewById(R.id.ImageButton02);
+			if( theGame.UpdateBoard(2) == true) 
+			{ 
+				image.setImageResource(Tic_or_Toe);
+							
+			}
+			break;
+		
+		case R.id.ImageButton03:
+			image = (ImageButton) findViewById(R.id.ImageButton03);
+			if( theGame.UpdateBoard(3) == true) 
+			{ 
+				image.setImageResource(Tic_or_Toe);
+						
+			}
+			break;
 			
+		case R.id.ImageButton04:
+			image = (ImageButton) findViewById(R.id.ImageButton04);
+			if( theGame.UpdateBoard(4) == true) 
+			{ 
+				image.setImageResource(Tic_or_Toe);
+								
+			}
+			break;
+			
+		case R.id.ImageButton05:
+			image = (ImageButton) findViewById(R.id.ImageButton05);
+			if( theGame.UpdateBoard(5) == true) 
+			{ 
+				image.setImageResource(Tic_or_Toe);
+								
+			}
+			break;
+			
+		case R.id.ImageButton06:
+			image = (ImageButton) findViewById(R.id.ImageButton06);
+			if( theGame.UpdateBoard(6) == true) 
+			{ 
+				image.setImageResource(Tic_or_Toe);			
+			}
+			break;
+			
+		case R.id.ImageButton07:
+			image = (ImageButton) findViewById(R.id.ImageButton07);
+			if( theGame.UpdateBoard(7) == true) 
+			{ 
+				image.setImageResource(Tic_or_Toe);
+								
+			}
+			break;
+			
+		case R.id.ImageButton08:
+			image = (ImageButton) findViewById(R.id.ImageButton08);
+			if( theGame.UpdateBoard(8) == true) 
+			{ 
+				image.setImageResource(Tic_or_Toe);				
+			}
+			break;
 
 		default:
 			break;
 		}
 		
+		//Check for winner or Draw
+		if(theGame.HasWinner()== true || theGame.BoardIsFull() == true)
+		{
+			if (theGame.WhoWon() == BoardFieldEnum.PLAYER_O)
+			{
+			Context context = getApplicationContext();
+			CharSequence text = "Pladen er fuld / der er fundes en vinder";
+			int duration = Toast.LENGTH_LONG;
+			
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+			
+			theGame.newGame();
+			
+			}
+			else
+			{
+				Context context = getApplicationContext();
+				CharSequence text = "Pladen er fuld / der er fundes en vinder";
+				int duration = Toast.LENGTH_LONG;
+				
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();
+			}
+		}
+		
+		theGame.changeTurn();
+		
+		//move = theGame.moveBot();
+		//String moveString = ("ImageButton0" + move);
+		//image = (ImageButton) findViewById(R.id.ImageButton00);
+		//int imageTag;
+		
+		image = (ImageButton) findViewById(imagearray[theGame.moveBot()]);
+		
+		image.setImageResource(R.drawable.bolle);	
+		
+		//Update GUI
+		//Check for winner or Draw
+		theGame.changeTurn();
     }
 }
