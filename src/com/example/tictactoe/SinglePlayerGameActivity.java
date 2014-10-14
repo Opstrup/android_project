be3bf1b90d5104017dbcce5040e05f1e1c840fef
Lydiksen <rasmus.lydiksen@gmail.com>
2014-10-14 12:36:39 +0200

    Singleplayergame opdateret.

--- src/com/example/tictactoe/SinglePlayerGameActivity.java
+++ src/com/example/tictactoe/SinglePlayerGameActivity.java
@@ -21,25 +21,62 @@ public class SinglePlayerGameActivity extends Activity
         
         String msg = gameIntent.getStringExtra(SinglePlayerOpponentActivity.GameType_Message);
         int gameTypeNumber = gameIntent.getIntExtra(SinglePlayerOpponentActivity.GameType_Message,1);
-        TextView opponentText = (TextView) findViewById(R.id.gameType2);
+        //TextView opponentText = (TextView) findViewById(R.id.gameType2);
         
         if(gameTypeNumber == 1)
         {
         	
-        	opponentText.setText("Random opponent");
+        //	opponentText.setText("Random opponent");
         }
         else
         {
-        	opponentText.setText("Terminator Opponent");
+        //	opponentText.setText("Terminator Opponent");
         }
     }
 	
 	
-	public void Setpiece(View view) {
+	public void SetButton(View view) {
 		
-		ImageButton image1 = (ImageButton) findViewById(R.id.ImageButton05);
+		/*
+		 ImageButton image00 = (ImageButton) findViewById(R.id.imageButton00);
+		 
     	
-		image1.setImageResource(R.drawable.kryds);
+		image00.setImageResource(R.drawable.kryds);
+		*/
+		
+		
+		switch (view.getId()) {
+		case R.id.ImageButton00:
+			ImageButton image00 = (ImageButton) findViewById(R.id.ImageButton00);
+			image00.setImageResource(R.drawable.kryds);
+			break;
+			
+		case R.id.ImageButton01:
+			ImageButton image01 = (ImageButton) findViewById(R.id.ImageButton01);
+			image01.setImageResource(R.drawable.kryds);
+			break;
+			
+	
+			
+			/*
+			 * Hvis tur er det?
+			 * - kryds/bolle
+			 * 		Hvis det er players tur:
+			 * 			set drawable til kryds eller bolle
+			 * 
+			 * Er pladsen ikke taget?
+			 * 
+			 * 		set billede på position.
+			 * 		void update.
+			 * Hvis plads er taget:
+			 * 		Ingen reaktion.
+			 * 
+			 */
+			
+
+		default:
+			break;
+		}
 		
     }
 }
