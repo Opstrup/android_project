e88ce5ba2df9a12bac07c75d9edd21f5718715ba
Lydiksen <rasmus.lydiksen@gmail.com>
2014-10-14 13:21:35 +0200

    Setbutton updated

--- src/com/example/tictactoe/SinglePlayerGameActivity.java
+++ src/com/example/tictactoe/SinglePlayerGameActivity.java
@@ -1,5 +1,11 @@
 package com.example.tictactoe;
 
+import com.example.tictactoe.GameLogic.BoardFieldEnum;
+import com.example.tictactoe.GameLogic.IBot;
+import com.example.tictactoe.GameLogic.ITicTacToeGame;
+import com.example.tictactoe.GameLogic.RandomBot;
+import com.example.tictactoe.GameLogic.TicTacToeGame;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +16,9 @@ import android.widget.TextView;
 
 public class SinglePlayerGameActivity extends Activity 
 {
+    ITicTacToeGame theGame;
+    IBot theBot;
+    int Tic_or_Toe;
     
 	@Override
     protected void onCreate(Bundle savedInstanceState) 
@@ -21,57 +30,60 @@ public class SinglePlayerGameActivity extends Activity
         
         String msg = gameIntent.getStringExtra(SinglePlayerOpponentActivity.GameType_Message);
         int gameTypeNumber = gameIntent.getIntExtra(SinglePlayerOpponentActivity.GameType_Message,1);
-        //TextView opponentText = (TextView) findViewById(R.id.gameType2);
+        TextView opponentText = (TextView) findViewById(R.id.gameType2);
         
         if(gameTypeNumber == 1)
         {
-        	
-        //	opponentText.setText("Random opponent");
+        	theBot = new RandomBot(BoardFieldEnum.PLAYER_X);
+        	theGame = new TicTacToeGame(9, theBot, BoardFieldEnum.PLAYER_O);
+        	opponentText.setText("Random opponent");
         }
         else
         {
-        //	opponentText.setText("Terminator Opponent");
+        	
+        	opponentText.setText("Terminator Opponent");
         }
     }
 	
+	public void GameLoop()
+	{
+//		while (theGame.HasWinner() == false && theGame.BoardIsFull == false) 
+//		{
+//			while(PlayerMove == false);
+//			
+//			
+//		}
+	}
 	
-	public void SetButton(View view) {
+	public void SetButton(View view) 
+	{
 		
-		/*
-		 ImageButton image00 = (ImageButton) findViewById(R.id.imageButton00);
-		 
-    	
-		image00.setImageResource(R.drawable.kryds);
-		*/
-		
-		
+		if (theGame.whosTurn() == BoardFieldEnum.PLAYER_O)
+		{
+			Tic_or_Toe = R.drawable.bolle;
+		}
+		else
+		{
+			Tic_or_Toe = R.drawable.kryds;
+		}
+				
 		switch (view.getId()) {
 		case R.id.ImageButton00:
 			ImageButton image00 = (ImageButton) findViewById(R.id.ImageButton00);
-			image00.setImageResource(R.drawable.kryds);
+//			if( theGame.updateBoard(0) == True) 
+//			{ 
+//				image00.setImageResource(Tic_or_Toe);
+//				
+//			}
+		
+
 			break;
 			
 		case R.id.ImageButton01:
 			ImageButton image01 = (ImageButton) findViewById(R.id.ImageButton01);
 			image01.setImageResource(R.drawable.kryds);
 			break;
-			
-	
-			
-			/*
-			 * Hvis tur er det?
-			 * - kryds/bolle
-			 * 		Hvis det er players tur:
-			 * 			set drawable til kryds eller bolle
-			 * 
-			 * Er pladsen ikke taget?
-			 * 
-			 * 		set billede på position.
-			 * 		void update.
-			 * Hvis plads er taget:
-			 * 		Ingen reaktion.
-			 * 
-			 */
+		
 			
 
 		default:
